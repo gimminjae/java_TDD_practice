811b5308b939701d3d8cdea9c7108c81e71ad194
gimminjae <min356812@naver.com>
2022-07-05 11:18:22 +0900

    add함수명 run으로 변경

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -6,20 +6,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
-    public void addTest() {
-        int result = Calculator.add("10 + 20");
+    public void add() {
+        int result = Calculator.run("10 + 20");
 
         assertEquals(30, result);
     }
     @Test
-    public void addTest_2() {
-        int result = Calculator.add("20 + 20");
+    public void add_2() {
+        int result = Calculator.run("20 + 20");
 
         assertEquals(40, result);
     }
     @Test
-    public void addTest_3() {
-        int result = Calculator.add("10 + 10");
+    public void add_3() {
+        int result = Calculator.run("10 + 10");
 
         assertEquals(20, result);
     }
